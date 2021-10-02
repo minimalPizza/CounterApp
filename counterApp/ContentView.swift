20035059de05fbdd1b4327db963338c1677b4c84
jdinwiddie <jdinwiddie@answersingenesis.org>
2021-10-01 17:53:41 -0700

    Initial Commit

---
+++ counterApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  counterApp
+//
+//  Created by John Dinwiddie on 10/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ counterApp/counterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterAppApp.swift
+//  counterApp
+//
+//  Created by John Dinwiddie on 10/1/21.
+//
+
+import SwiftUI
+
+@main
+struct counterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
